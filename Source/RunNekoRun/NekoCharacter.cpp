d57c48030d38d33aea0beffaed57919408279ac9
xena99 <xena.stranger99@gmail.com>
2024-08-21 15:43:13 -0700

    Init

--- Source/RunNekoRun/NekoCharacter.cpp
+++ Source/RunNekoRun/NekoCharacter.cpp
@@ -2,6 +2,11 @@
 
 
 #include "NekoCharacter.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "Landscape.h"
 
 // Sets default values for this component's properties
@@ -19,7 +24,7 @@ UNekoCharacter::UNekoCharacter()
 void UNekoCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
+	CharacterOwner = Cast<ACharacter>(GetOwner());
 	// ...
 	
 }
@@ -29,12 +34,22 @@ void UNekoCharacter::BeginPlay()
 void UNekoCharacter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	AlignToGround();
+
+	//Vault();
+
+}
+
+void UNekoCharacter::AlignToGround()
+{
+	//Align the Character to the ground
 	FVector Start = GetOwner()->GetActorLocation();
-	FVector End = Start + FVector(0, 0, MaxDistance);
-	FHitResult Hit;
+	FVector End = Start + FVector(0, 0, GroundDIstance);
 	FCollisionQueryParams CollisionParams;
+	FHitResult Hit;
 	CollisionParams.AddIgnoredActor(GetOwner());
-	if(GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, CollisionParams))
+	if (GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, CollisionParams))
 	{
 		FVector GroundNormal = Hit.ImpactNormal;
 		FVector ForwardVector = GetOwner()->GetActorForwardVector();
@@ -48,3 +63,96 @@ void UNekoCharacter::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 	}
 }
 
+void UNekoCharacter::VaultOrClimb()
+{
+	FVector Start = CharacterOwner->GetActorLocation();
+	FVector End = Start + CharacterOwner->GetActorForwardVector() * VaultDistance;
+
+	FCollisionObjectQueryParams ObjectsToTrace;
+	ObjectsToTrace.AddObjectTypesToQuery(ECollisionChannel::ECC_WorldStatic);
+	FCollisionQueryParams ActorsToIgnore;
+	ActorsToIgnore.AddIgnoredActor(CharacterOwner);
+
+	//First Line Trace to detect the wall
+	FHitResult Hit;
+
+	DrawDebugLine(GetWorld(), Start, End, FColor::Blue, false, 5.0f);
+
+	GetWorld()->LineTraceSingleByObjectType(Hit, Start, End, ObjectsToTrace, ActorsToIgnore);
+	if (Hit.Component == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Hit Component Found"));
+		//no wall detected
+		return;
+	}
+
+	WallNormal = Hit.Normal;
+	WallHitLocation = Hit.Location;
+
+	WallRotator = UKismetMathLibrary::MakeRotFromX(WallNormal);
+	WallForwardVector = UKismetMathLibrary::GetForwardVector(WallRotator);
+
+	//Get Wall Height
+	Start = WallHitLocation + FVector(0, 0, VaultDistance) + WallForwardVector * -10.f;
+	End = Start - FVector(0, 0, VaultDistance);
+
+	DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 5.0f);
+
+	GetWorld()->LineTraceSingleByObjectType(Hit, Start, End, ObjectsToTrace, ActorsToIgnore);
+	WallHeight = Hit.Location;
+
+	//Get Wall Thickness
+	Start = WallHitLocation + FVector(0, 0, VaultDistance + 50.f) + WallForwardVector * -40.f;
+	End = Start - FVector(0, 0, VaultDistance + 200.f);
+
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red, false, 5.0f);
+
+	GetWorld()->LineTraceSingleByObjectType(Hit, Start, End, ObjectsToTrace, ActorsToIgnore);
+	WallHeight2 = Hit.Location;
+
+	WallThickness = WallHeight - WallHeight2;
+
+	UE_LOG(LogTemp, Warning, TEXT("Heihgt: %f Thickness: %f"), WallHeight.Z, WallThickness.Z);
+
+	if (fabs(WallHeight.Z) <= VaultHeightThreshold && fabs(WallThickness.Z) <= VaultWidthThreshold)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Vaulting"));
+
+		//CharacterOwner->GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		CharacterOwner->GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+
+		FVector PositionInFrontOfWall = CharacterOwner->GetActorLocation() + WallForwardVector * 20;
+		float TargetZ = WallHeight.Z + JumpMargin;
+
+		FVector FinalPosition = FVector(PositionInFrontOfWall.X, PositionInFrontOfWall.Y, TargetZ);
+		CharacterOwner->SetActorLocation(FinalPosition);
+
+		//Play Vault Animation
+		UAnimInstance* AnimInstance = CharacterOwner->GetMesh()->GetAnimInstance();
+		if (AnimInstance && VaultMontage)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Play Vaulting"));
+
+			AnimInstance->Montage_Play(VaultMontage, 1.0f);
+
+			float MontageDuration = VaultMontage->GetPlayLength();
+
+			// Setup a timer to reset collision and rotation after the animation
+			FTimerHandle TimerHandle;
+			GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UNekoCharacter::ResetActorState, MontageDuration, false);
+		}
+	}
+}
+
+void UNekoCharacter::ResetActorState()
+{
+	CharacterOwner->GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CharacterOwner->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+
+	FRotator ActorRotator = WallRotator;
+	ActorRotator.Yaw += 180;
+	ActorRotator.Roll = CharacterOwner->GetActorRotation().Roll;
+	ActorRotator.Pitch = CharacterOwner->GetActorRotation().Pitch;
+
+	//CharacterOwner->SetActorRotation(ActorRotator);
+}
--- Source/RunNekoRun/NekoCharacter.h
+++ Source/RunNekoRun/NekoCharacter.h
@@ -7,7 +7,7 @@
 #include "NekoCharacter.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS( ClassGroup = (Custom), meta = (BlueprintSpawnableComponent) )
 class RUNNEKORUN_API UNekoCharacter : public UActorComponent
 {
 	GENERATED_BODY()
@@ -16,16 +16,58 @@ public:
 	// Sets default values for this component's properties
 	UNekoCharacter();
 
+
 protected:
+
+	UPROPERTY(EditAnywhere, Category = "NekoCharacter")
+	float GroundDIstance = -100.0f;
+
+	UPROPERTY(EditAnywhere, Category = "VaultingDistance")
+	float VaultDistance = 200.0f;
+
+	UPROPERTY(EditAnywhere, Category = "VaultHeightThreshold")
+	float VaultHeightThreshold = 200.0f;
+
+	UPROPERTY(EditAnywhere, Category = "VaultWidthThreshold")
+	float VaultWidthThreshold = 200.0f;
+
+	UPROPERTY(EditAnywhere, Category = "JumpMargin")
+	float JumpMargin = 20.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Vault Montage")
+	UAnimMontage* VaultMontage;
+
+	FVector WallThickness;
+
+	FVector WallHeight;
+
+	FVector WallHeight2;
+
+	FVector WallNormal;
+
+	FVector WallHitLocation;
+
+	FVector WallForwardVector;
+
+	FRotator WallRotator;
+
+	ACharacter* CharacterOwner;
+
 	// Called when the game starts
 	virtual void BeginPlay() override;
-	UPROPERTY(EditAnywhere, Category = "NekoCharacter")
-	float MaxDistance = -100.0f;
-	
+
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	void AlignToGround();
+
+	UFUNCTION(BlueprintCallable, Category = "Vault")
+	void VaultOrClimb();
+
+
+	void ResetActorAfterVault();
+	
+	void ResetActorState();
 };
